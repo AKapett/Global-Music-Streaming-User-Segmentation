182dc0adb6df0f18639e2c02847ea3f568bea108
Aleksandar Kapetanovic <contactkapett@gmail.com>
2025-04-05 15:13:01 -0400

    Update 04_segmentation_flags.sql
    added QA check queries

--- 04_segmentation_flags.sql
+++ 04_segmentation_flags.sql
@@ -1,3 +1,4 @@
+-- Create segmentes to establish easy churn flag parameters
 -- Add three segmentation columns at the end of the table
 ALTER TABLE users_cleaned
   ADD COLUMN engagement_level VARCHAR(10),
@@ -31,6 +32,23 @@ SET upgrade_potential_flag = CASE
 END;
 
 
+-- QA CHECKS
+-- QA CHECK: engagement level
+-- 'High' but streamed < 500
+SELECT * FROM users_cleaned 
+WHERE engagement_level = 'High' AND minutes_streamed_per_day < 500;
+
+-- 'Medium' but not between 250 and 499.999...
+SELECT * FROM users_cleaned 
+WHERE engagement_level = 'Medium' 
+  AND (minutes_streamed_per_day < 250 OR minutes_streamed_per_day >= 500);
+
+-- 'Low' but streamed ≥ 250
+SELECT * FROM users_cleaned 
+WHERE engagement_level = 'Low' AND minutes_streamed_per_day >= 250;
+
+
+-- ESTABLISH CHURN
 -- Add churn_risk_flag column
 ALTER TABLE users_cleaned
 ADD COLUMN churn_risk_flag VARCHAR(10);
@@ -54,3 +72,34 @@ SET churn_risk_flag = CASE
     -- Low risk: Everyone else (Medium/High engagement or Loyal behavior)
     ELSE 'Low'
 END;
+
+
+-- QA CHECK
+-- QA 1: High risk users who don’t match high-risk conditions
+SELECT * FROM users_cleaned
+WHERE churn_risk_flag = 'High'
+  AND NOT (
+    engagement_level = 'Low' AND 
+    (
+      repeat_song_rate < 20 OR
+      number_of_songs_liked < 5 OR
+      discover_weekly_engagement < 20
+    )
+  );
+
+-- QA 2: Moderate risk users missing valid moderate pattern
+SELECT * FROM users_cleaned
+WHERE churn_risk_flag = 'Moderate'
+  AND NOT (
+    engagement_level = 'Low' AND 
+    repeat_song_rate >= 20 AND
+    number_of_songs_liked >= 5 AND
+    discover_weekly_engagement >= 20
+  );
+
+-- QA 3: Low risk users who might still meet high or moderate risk rules
+SELECT * FROM users_cleaned
+WHERE churn_risk_flag = 'Low'
+  AND (
+    engagement_level = 'Low'
+  );
